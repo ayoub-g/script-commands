39898005b73dbde594e864dd98ba5a144ba9e727
Ayoub.Gharbi <gharbi.ayoub@gmail.com>
2021-08-24 18:04:24 +0100

    add open-dev-projects-folder script

---
+++ commands/system/open-dev-projects-folder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open dev-projects Folder
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName System
+
+# Documentation:
+# @raycast.description Open dev-projects Folder
+# @raycast.author Ayoub Gharbi
+# @raycast.authorURL github.com/ayoub-g
+
+
+
+	open "~/dev-projects"
